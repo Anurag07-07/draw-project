0967422e6d487fee06e7a4107260b2f8eb90b962
Anurag Raj <adityanv8@gmail.com>
2025-11-07 12:57:05 +0530

    dotenv conectded completd

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,38 +1,40 @@
 import {WebSocketServer} from 'ws'
 import jwt from 'jsonwebtoken'
 const wss = new WebSocketServer({port:8080})
+import dotenv from 'dotenv'
+import { string } from 'zod'
+dotenv.config()
 
 interface JwtPayload{
   _id:string
 }
 
-function checkUser(token:string){
-  const decoded = jwt.verify(token,process.env.JWT_SECRET as unknown as string) as JwtPayload  
-
-  if (typeof decoded !== 'string') {
+function checkUser(token:string):string | null{
+  const decoded = jwt.verify(token,process.env.JWT_SECRET as string) as string | jwt.JwtPayload
+  if (typeof decoded === 'string' || !decoded) {
     return null
   }
 
-  if (!decoded) {
-    return null
-  }
-
-  console.log(token);
-  
-
-  // return decoded._id
-
+  const payload = decoded as JwtPayload
+  return payload._id
 }
 
-wss.on('connection',(socket,request)=>{
-  // const url = request.url
-  // if (!url) {
-  //   throw new Error(`Url is Not Defined`)
-  // }
 
-  // const searchparams = new URLSearchParams(url.split("?")[1])
-  // const token = searchparams.get('token') || ""
-  // const present = checkUser(token)
-  console.log("Connection");
-  
+wss.on('connection',(socket,request)=>{
+  const url = request.url
+  if (!url) {
+    throw new Error(`Url is Not Defined`)
+  }
+
+  const searchparams = new URLSearchParams(url.split("?")[1])
+  const token = searchparams.get('token') || ""
+  const userId = checkUser(token)
+
+  if (!userId) {
+    socket.close()
+  }
+
+  socket.on('message',(data)=>{
+    socket.send('pong') 
+  })
 })
