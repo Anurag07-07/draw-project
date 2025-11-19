32b5fcfc76362cc65b1ea1a850f99e81e1a94ba7
Anurag Raj <adityanv8@gmail.com>
2025-11-19 16:02:19 +0530

    middleware added

---
+++ apps/docs/proxy.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher:['/',"/signin","/signup","/dashboard/:path*"]
+}
+
+export default async function proxy(req:NextRequest,res:NextResponse){
+  const token = req.cookies.get('token')?.value
+  const publicRoutes = ["/signup","/signin"]
+  const isPublic = publicRoutes.includes(req.nextUrl.pathname)
+  if (!token && !isPublic) {
+    return NextResponse.redirect(new URL('/signup',req.url))
+  }
+  if (token && isPublic) {
+    return NextResponse.redirect(new URL('/',req.url))
+  }
+  return NextResponse.next()
+}
+
