32ada8cce6ab2bcd18c7b9502ddb22e15f0e96c1
Anurag07-07 <adityanv8@gmail.com>
2025-11-26 21:34:41 +0530

    feat: Initialize HTTP backend server with Express, CORS, cookie-parser, and user routes.

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser())
 
 app.use('/api/v1',userrouter)
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT,()=>{
   console.log(`Server Run at Port ${PORT}`);
